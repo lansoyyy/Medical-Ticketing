cd3af455db109dab989c0184790f0a1b482a22df
Lans12345 <olanalans12345@gmail.com>
2025-12-09 06:10:57 +0800

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:medical_ticketing/screens/admin/admin_home_screen.dart';
 import 'package:medical_ticketing/screens/doctor/doctor_home_screen.dart';
@@ -6,9 +7,19 @@ import 'package:medical_ticketing/screens/patient/patient_home_screen.dart';
 import 'config/app_theme.dart';
 import 'screens/auth/login_screen.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MyApp());
+
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyAp_5W0O_nSGFihfn-2sbh12VtSBbA0jNI",
+          authDomain: "medical-ticketing.firebaseapp.com",
+          projectId: "medical-ticketing",
+          storageBucket: "medical-ticketing.firebasestorage.app",
+          messagingSenderId: "177236370054",
+          appId: "1:177236370054:web:f02fdd19f91ecaf20f2707"));
+
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
